f19452ba4529e16bd2e8f1d4b67b6d3a197fe1fa
Jhony Alceu Pereira <jhonystein@gmail.com>
2017-11-10 16:55:58 -0200

    Ajustes para o Openshift

--- index.js
+++ index.js
@@ -58,6 +58,6 @@ app.get('/', function(req, resp) {
 })
 
 http.createServer(app)
-  .listen(process.env.PORT || 8080, function(){
+  .listen(process.env.OPENSHIFT_NODEJS_PORT || 8080, process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1', function(){
     console.log('Servidor iniciado na porta', process.env.PORT || 8080)
   })
