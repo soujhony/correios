b1af7bf1cfb02949d392ece4adf8124b4b833ec0
Jhony Alceu Pereira <jhonystein@gmail.com>
2017-12-20 08:49:34 -0200

    ajustes do teste

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const swaggerSpec = swaggerJSDoc({
       version: '1.0.0',
       description: 'Catalogo dos métodos',
     },
-  basePath: '/',
+  basePath: '/api',
   },
   apis: ['./index.js']
 })
@@ -42,7 +42,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
 /**
  * @swagger
- * /api/cep:
+ * /cep/{codigo}:
  *   get:
  *     description: Consulta um determinado CEP
  *     produces:
